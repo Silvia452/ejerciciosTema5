a49a75066dc77d394fbd988646cd15a451403995
Silvia Lorero Salor <1004291182alumnos.uc3m.es>
2023-02-22 12:59:14 +0100

    Falta ejercicio 6

--- src/main/java/ejercicio4.java
+++ src/main/java/ejercicio4.java
@@ -2,10 +2,48 @@ import java.util.Scanner;
 
 public class ejercicio4 {
     public static void main(String[] args) {
+        maquinaVotar();
 
     }
 
     public static void maquinaVotar(){
+        Scanner sn = new Scanner(System.in);
+        int votos[] = new int[4];
+        System.out.println("Elija entre:\n" +
+                "1 - Emilia TERREROS\n" +
+                "2 - Gerardo ESCODAR\n" +
+                "3 - María AGUAVIVA\n" +
+                "4 - Nadia LETUX");
+
+        int voto = sn.nextInt();
+
+        while (voto != 68753421){
+            System.out.println("Elija entre:\n" +
+                    "1 - Emilia TERREROS\n" +
+                    "2 - Gerardo ESCODAR\n" +
+                    "3 - María AGUAVIVA\n" +
+                    "4 - Nadia LETUX");
+        }
+        if (voto ==1) {
+            votos[0]++;
+        }
+        else if (voto ==2) {
+            votos[1]++;
+        }
+        else if (voto ==3) {
+            votos[2]++;
+        }
+        else if (voto ==4) {
+            votos[3]++;
+        }
+        else if (voto ==68753421) {
+            System.out.println("Resultados:\n" +
+                    "1 - Emilia TERREROS:\n" + votos[0] +
+                    "2 - Gerardo ESCODAR\n" + votos[1] +
+                    "3 - María AGUAVIVA\n" + votos[2] +
+                    "4 - Nadia LETUX" + votos[3]);
+        }
+
 
 
 
--- src/main/java/ejercicio7.java
+++ src/main/java/ejercicio7.java
@@ -1,8 +1,24 @@
+//EJERCICIO 7
+//Crear una tabla de caracteres bidimensional de tamaño 20×30, en la que se colocan caracteres elegidos al azar. Mostrar la tabla generada.
 public class ejercicio7 {
+
     public static void main(String[] args) {
+        matriz();
 
     }
     public static void matriz(){
+        char[][] matriz = new char[20][30];
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = (char) (Math.random() * 26 + 'a');
+            }
+        }
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
 
     }
 }
