d73e797286b8a944413b79ff4d33b45732190450
Silvia Lorero Salor <1004291182alumnos.uc3m.es>
2023-02-27 12:22:55 +0100

    Ejercicio 8 completado

--- src/main/java/ejercicio8.java
+++ src/main/java/ejercicio8.java
@@ -6,12 +6,34 @@
 //
 //Solo hay un barco que ocupa una única celda (este espacio se elige al azar).
 
+import java.util.Scanner;
+
 public class ejercicio8 {
+
+    public static final Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
 
     }
     public static void microGuerraBarcos(){
         int[][] tablero = new int[4][4];
+        int fila = (int) (Math.random() * 4);
+        int columna = (int) (Math.random() * 4);
+        tablero[fila][columna] = 1;
+        int fila2;
+        int columna2;
+        int cont = 0;
+        do {
+            System.out.println("Introduce una fila: ");
+            fila2 = sc.nextInt();
+            System.out.println("Introduce una columna: ");
+            columna2 = sc.nextInt();
+            if (fila2 == fila && columna2 == columna) {
+                System.out.println("Has hundido el barco");
+                cont++;
+            } else {
+                System.out.println("Has fallado");
+            }
+        } while (cont == 0);
 
     }
 }
