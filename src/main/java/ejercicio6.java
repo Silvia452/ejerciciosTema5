0481a4db8d375b73c652f2bc2deffd55a274d8a8
Silvia Lorero Salor <1004291182alumnos.uc3m.es>
2023-02-27 15:45:21 +0100

    Todos ejercicios completados

--- src/main/java/ejercicio6.java
+++ src/main/java/ejercicio6.java
@@ -1,12 +1,76 @@
 public class ejercicio6 {
 
     public static void main(String[] args) {
-        ejer6();
+        ejercicio6metodo1();
+        ejercicio6metodo2();
+        ejercicio6metodo3();
+
     }
 
-    public static void ejer6(){
+    public static void ejercicio6metodo1() {
+        final int TAMANO = 3;
+        int i, j, val;
+        int[][] tab2d = new int[TAMANO][TAMANO];
+        val = 1;
+        for (j = 0; j < TAMANO; j++) {
+            for (i = 0; i < TAMANO; i++) {
+                tab2d[j][i] = val;
+                val = val + 1;
+            }
+        }
 
+        for (j = 0; j < TAMANO; j++) {
+            for (i = 0; i < TAMANO; i++) {
+                System.out.print(tab2d[j][i] + " ");
+            }
+            System.out.println();
+        }
     }
+
+    public static void ejercicio6metodo2() {
+        final int TAMANO = 4;
+        int i, j, val;
+        int[][] tab2d = new int[TAMANO][TAMANO];
+        val = 1;
+        for (j = 0; j < TAMANO; j++) {
+            for (i = 0; i < TAMANO; i++) {
+                tab2d[j][i] = val;
+                val = val + 1;
+            }
+        }
+
+        //Mostrar la tabla
+        for (j = 0; j < TAMANO; j++) {
+            for (i = 0; i < TAMANO; i++) {
+                System.out.print(tab2d[j][i] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void ejercicio6metodo3() {
+        final int TAMANO = 4;
+        int i, j, val;
+        int[][] tab2d = new int[TAMANO][TAMANO];
+        val = 0;
+        for (j = 0; j < TAMANO; j++) {
+            for (i = 0; i < TAMANO; i++) {
+                tab2d[j][i] = val;
+                val = val + 1;
+            }
+            val = j+1;
+        }
+
+        //Mostrar la tabla
+        for (j = 0; j < TAMANO; j++) {
+            for (i = 0; i < TAMANO; i++) {
+                System.out.print(tab2d[j][i] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
 
 
 }
